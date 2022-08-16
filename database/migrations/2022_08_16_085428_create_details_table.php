e9cc8b790e319285f8d56600a8665014b4e91268
Zara Bostock <bostock.zara@gmail.com>
2022-08-16 10:26:32 +0100

    Database migration files added and edited, models and controllers also added

---
+++ database/migrations/2022_08_16_085428_create_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('address3');
+            $table->string('postcode');
+            $table->integer('phone');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+};
